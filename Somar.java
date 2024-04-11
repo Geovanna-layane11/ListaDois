8614354b518ee27b1e67695bc1fa9785315eaee6
Geovanna-layane11 <166646464+Geovanna-layane11@users.noreply.github.com>
2024-04-11 18:04:50 -0300

    Create Somar.java

---
+++ Somar.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+	public class Somar {
+	public static void main(String[]args) {;
+		Scanner scan  = new Scanner (System.in);
+		System.out.println ("escreva um numero: ");
+		int a = scan.nextInt ();
+		System.out.println ("escreva um numero: ");
+		int b = scan.nextInt ();
+		
+		System.out.println ( a + b);
+	}
+	
+
+}
